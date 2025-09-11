e2f6ae18c020ca29c4cc1c6556b4ef5e7cea2752
Dharmagisha <dharmagisham@gmail.com>
2025-09-11 16:17:02 +0530

    time change

--- src/TimeFind.jsx
+++ src/TimeFind.jsx
@@ -8,7 +8,7 @@ const TimeFind = () => {
 
        const times = time.split(":").map(Number);
 
-        if (times.length !== 1) {
+        if (times.length !== 2) {
             setShow("Please enter in HH:MM format");
             return;
         }
