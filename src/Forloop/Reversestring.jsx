e0d4321287e599e13b881844d866314881fbdfcd
Dharmagisha <dharmagisham@gmail.com>
2025-09-12 15:53:59 +0530

    reverse string in loop

--- src/Forloop/Reversestring.jsx
+++ src/Forloop/Reversestring.jsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 
 const Reversestring = () => {
     const [text, setText] = useState('');
-    const reverse = (str) => str.split("").reverse().join("");
+    
+         
+    let reverse = "";
+    for( let i = text.length-1; i>=0;i--){
+        reverse += text[i];
+    }
+
 
   return (
     <div>
         <h3>REVERSED STRING</h3>
         <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
         <p>{text}</p>
-         <p>Reversed:{reverse(text)}</p>
+         <p>Reversed:{reverse}</p>
       
     </div>
   )
