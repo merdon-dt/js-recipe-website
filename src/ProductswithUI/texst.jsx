6e555602cd3bc0d5302059700a665940deddafdc
Dharmagisha <dharmagisham@gmail.com>
2025-10-04 11:36:59 +0530

    git Incart push

---
+++ src/ProductswithUI/texst.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const texst = () => {
+
+    const products = [ 
+        {
+            name: "skincare"
+        }
+    ]
+  return (
+    <div>
+
+
+
+
+    </div>
+  )
+}
+
+export default texst
